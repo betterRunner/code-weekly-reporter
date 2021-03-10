3c6a9de852ed6ebdf916c8567444d80c721900c6
betterRunner <neochannel@163.com>
2021-03-10 14:41:17 +0800

    chore: tailwind

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { createApp } from 'vue';
 import ElementPlus from 'element-plus'
 import 'element-plus/lib/theme-chalk/index.css'
 import App from './App';
+import './index.css';
 import router from './router';
 import store from './store';
 import '@/style/index.scss';
